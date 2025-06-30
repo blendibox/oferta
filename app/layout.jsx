5dfeb45392d6f04a3eab6cab56b10cec4c4e4f23
blendibox <loja@blendibox.com.br>
2025-06-29 22:37:00 -0300

    ajustes SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 	<title>Dicas de Compras Blendibox - Qual o melhor lugar para comprar?</title>
+	<meta name="description" content="Bolsas 3 em 1, mochilas e acessórios pensados para mulheres práticas. Na Blendibox você encontra estilo, funcionalidade e qualidade com preço justo">
+	</meta>
       <body>
         {children}
       </body>
