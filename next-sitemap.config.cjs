8d0e680074b354610df97384360ad052abcd669a
blendibox <loja@blendibox.com.br>
2025-07-18 04:04:02 -0300

    ajustes sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl:  'https://comprar.blendibox.com.br',
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 5000, // Divide o sitemap em múltiplos arquivos a cada 5000 URLs
-  exclude: ["/404"],
-   robotsTxtOptions: {
+  generateRobotsTxt: false,
+  sitemapSize: 1000,
+   exclude: [
+	  "/404",
+	  "/*/__dummy__"
+	],
+  outDir: './out',
+  robotsTxtOptions: {
     policies: [
-      {
-        userAgent: "*",
-        disallow: ["/404"],
-      },
+      { userAgent: "*", disallow: ["/404"] },
       { userAgent: "*", allow: "/" },
     ]
   },
