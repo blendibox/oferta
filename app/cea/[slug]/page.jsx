d4c69111b18c3fc75f556690e90c59997baac473
blendibox <loja@blendibox.com.br>
2025-07-27 15:37:07 -0300

    add cea again

---
+++ app/cea/[slug]/page.jsx
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+import { lerProdutoPorSlug } from '../../../lib/awin';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import ProdutoAwin from '../../../components/produtoAwin';
+
+export async function generateStaticParams() {
+	
+	
+   if (process.env.BUILD_TARGET !== 'cea') {
+    return [{ slug: '__dummy__' }]; // ⚠️ slug fake para evitar erro no build
+  } 
+	
+     
+   const nomeArquivo = process.env.SLUGS_FILE;
+
+  const indexPath = path.join(process.cwd(), 'data', 'slugs-lotes', nomeArquivo);
+  const slugsProduto = [];
+  
+  const linhas = fs.readFileSync(indexPath, 'utf8').split('\n');
+  const slugs = [];
+
+  for (const linha of linhas) {
+    if (!linha.trim()) continue; // Ignora linhas vazias
+    try {
+		
+      const obj = JSON.parse(linha);
+      if (obj.slug) {
+        slugs.push({ slug: obj.slug });
+      }
+    } catch (e) {
+      console.warn(`❌ Erro ao parsear linha: ${linha}`);
+    }
+  }
+
+  return slugs;
+ 
+ 
+}
+
+export async function generateMetadata({ params }) {
+  const lote = process.env.LOTE || null;
+  const produto = await lerProdutoPorSlug(params.slug, 'CEA',lote);
+
+
+  if (!produto) return {};
+
+  return {
+    title: produto['text']['name'],
+    description: produto['text']['desc'],
+  };
+}
+
+
+
+export default async function ProdutoPage({ params }) {
+    
+  const lote = process.env.LOTE || null;
+  const produto = await lerProdutoPorSlug(params.slug, 'CEA',lote);
+
+  if (!produto) return notFound();
+
+  return (
+	  <ProdutoAwin
+		produto= {produto}
+		mybrand=' CEA'
+	  >
+	  </ProdutoAwin>
+	  
+  );
+}
